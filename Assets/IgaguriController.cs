c8631255152e70b79fc5aaed5c5478790de08886
kouheigit <ookawakouhei777@gmail.com>
2022-11-21 20:41:09 +0900

    igaguri add force

--- Assets/IgaguriController.cs
+++ Assets/IgaguriController.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,19 +5,15 @@ using UnityEngine;
 public class IgaguriController : MonoBehaviour
 {
     // Start is called before the first frame update
-    public void Shoot(Vector3 dir)
+    public void Shoot(Vector3  dir)
     {
         GetComponent<Rigidbody>().AddForce(dir);
-    }
-    private void OnCollisionEnter(Collision other)
-    {
-        GetComponent<Rigidbody>().isKinematic = true;
-        GetComponent<ParticleSystem>().Play();
     }
 
     // Update is called once per frame
     void Start()
     {
         Application.targetFrameRate = 60;
+        Shoot(new Vector3(0, 200, 2000));
     }
 }
